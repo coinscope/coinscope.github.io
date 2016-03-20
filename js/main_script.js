5c9c10d14e266c943b88d06b21b4659c2b034e7f
Dmitri <coinscope@gmail.com>
2016-03-21 00:16:06 +0200

    update 2.0
    all static styles are made; some type of slider is workin

---
+++ js/main_script.js
@@ -0,0 +1,11 @@
+﻿jQuery(document).ready(function(){
+	$("#offers ul li:nth-child(odd)").addClass("left");	
+	$('.form form input[type="text"]').focus(function(){
+		value_f=$(this).val();
+		$(this).removeAttr('value');
+	}).blur(function(){
+		if($(this).val()==''){
+		$(this).val(value_f)}
+	});
+	
+});
